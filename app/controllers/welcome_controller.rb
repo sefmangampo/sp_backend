9f752df51d420c68aeb3cdabc387ac537fc7965c
sef mangampo <sefmangampo@gmail.com>
2021-10-16 18:21:31 +0800

    added welcome details

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,18 @@
 class WelcomeController < ApplicationController
   def index
-    render json: {message: 'Hello world'}
+    render json: {
+      message: 'Hello world',
+      github: 'https://github.com/sefmangampo/sp_backend',
+      endpoints: {
+        post: {
+          authenticate: "api/authenticate",
+          authorize: "api/authorize",
+          create_user: "api/users"
+        },
+        get: {
+          get_all_users: "api/users"
+        }
+      }
+    }
   end
 end
